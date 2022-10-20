0c4cb14dcb3a0c9dc5d7b6f15b22b5cad69043c9
Hugo Delcey <delcey.hugo@outlook.fr>
2022-10-20 06:29:41 +0200

    first commit

---
+++ src/components/properties/Main.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="content">
+        <h1>{{ name }}</h1>
+        <div class="display happiness">
+            <h2>Happiness</h2>
+            <img src="" alt="">
+            <h2>{{ happiness }}</h2>
+        </div>
+        <div class="display income">
+            <h2>Income</h2>
+            <img src="" alt="">
+            <h2>{{ income }}</h2>
+        </div>
+        <div class="display sell-price">
+            <h2>Sell</h2>
+            <img src="" alt="">
+            <h2>{{ sellPrice }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    name: String,
+    happiness: Number,
+    income: Number,
+    sellPrice: Number
+})
+</script>
+
+<style>
+.content{
+    display: flex;
+    flex-direction: column;
+}
+.name{
+    grid-area: title;
+}
+.display{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+    "title title";
+    background-color: #F8F5F2;
+    border-radius: 5px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ src/components/properties/Properties.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class="card">
+        <ul id="sections">
+            <li class="section"></li>
+            <li class="section"></li>
+            <li class="section"></li>
+        </ul>
+        <main>
+            <div class="close">
+                
+            </div>
+            <div class="content">
+              <Home />
+            </div>
+        </main>
+    </div>
+
+
+
+
+  
+</template>
+
+<script setup>
+import Home from './Main.vue'
+import Special from './Special.vue'
+import Option from './Option.vue'
+defineProps({
+  cost: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style>
+#sections{
+  display: flex;
+  flex-direction: row;
+}
+.section{
+  list-style: none;
+
+}
+.content{
+  background-color: #FFFFFE;
+}
+</style>
